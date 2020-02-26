9d69c7358e33f94eb27e588bb702341795f4f001
Pavel Holec <pavel.holec@gmail.com>
2020-02-26 19:12:40 +0100

    Ability to search in meteorite list - basic ui

--- Crash Map/Views/MeteoriteListTableViewController.swift
+++ Crash Map/Views/MeteoriteListTableViewController.swift
@@ -5,23 +5,27 @@ class MeteoriteListTableViewController: UITableViewController {
     
     let meteoriteService = MeteoriteService()
     
+    var filteredMeteorites: [Meteorite] = []
+    
     // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSearchController()
     }
     
     // MARK: - TableView
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return meteoriteService.meteorites.count
+        isFiltering ? filteredMeteorites.count : meteoriteService.meteorites.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let meteoriteCell = tableView.dequeueReusableCell(withIdentifier: "Meteorite Cell") as! MeteoriteListTableViewCell
-        meteoriteCell.configure(meteorite: meteoriteService.meteorites[indexPath.row])
+        let meteorite = isFiltering ? filteredMeteorites[indexPath.row] : meteoriteService.meteorites[indexPath.row]
+        meteoriteCell.configure(meteorite: meteorite)
         return meteoriteCell
     }
     
@@ -33,5 +37,35 @@ class MeteoriteListTableViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+    }
+    
+    // MARK: - Private
+    private func addSearchController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search for Meteorite..."
+        
+        navigationItem.searchController = searchController
+    }
+}
+
+// MARK: - Search
+extension MeteoriteListTableViewController: UISearchResultsUpdating {
+    
+    var isSearchBarEmpty: Bool {
+        navigationItem.searchController!.searchBar.text?.isEmpty ?? true
+    }
+    
+    var isFiltering: Bool {
+        navigationItem.searchController!.isActive && !isSearchBarEmpty
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchTerm = searchController.searchBar.text else {
+            return
+        }
+        
+        tableView.reloadData()
     }
 }
